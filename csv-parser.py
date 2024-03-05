89dcee565d012f8d8d81c2a3072c8fada35bd42a
KentQuilestino <22610@burnside.school.nz>
2024-03-05 13:39:58 +1300

    Testing out SQLalchemy

--- csv-parser.py
+++ csv-parser.py
@@ -1,9 +1,32 @@
 import csv
+from sqlalchemy import create_engine,  text
 
-with open('steam.csv', mode="r") as csv_file:
-    reader = csv.reader(csv_file)
-    try:
-        for row in reader:
-            print(row)
-    except UnicodeDecodeError:
-        pass
+engine = create_engine("sqlite:///SteamData_Dummy.db",
+                       future=True, echo=True)
+
+conn = engine.connect()
+
+result = conn.execute(text("SELECT gameName FROM Games"))
+
+print(result.all())
+
+
+
+
+# with open('steam.csv', mode="r") as csv_file:
+#     reader = csv.reader(csv_file)
+#     for row in reader:
+#         if row[0].isdigit():
+#             pass
+            
+            # cur.execute("""INSERT INTO Games (gameID, gameName, releaseDate,
+            #             languageEnglish, requiredAge, gameAchievements,
+            #             positiveRatings, negativeRatings, averagePlaytime,
+            #             medianPlaytime, gamePrice)
+            #             Values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""",
+            #             (row[0], row[1], row[2], row[3], row[7], row[11],
+            #              row[12], row[13], row[14], row[15], row[17]))
+            # connection.commit()
+
+        
+
