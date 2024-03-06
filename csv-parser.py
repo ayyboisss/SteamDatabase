7a4b9e29a45b4fab6fe69aff3fbd226a48690ae7
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-03-07 09:27:06 +1300

    no more sqlalchemy, back to sqlite3

--- csv-parser.py
+++ csv-parser.py
@@ -1,33 +1,57 @@
 import csv
-from sqlalchemy import create_engine, insert
-from sqlalchemy.orm import Session
-from sqlalchemy.ext.automap import automap_base
+import sqlite3
 
-Base = automap_base()
-engine = create_engine("sqlite:///SteamData_Dummy.db",
-                       future=True, echo=True)
-# This took way too long to figure out
-Base.prepare(autoload_with=engine)
-Games = Base.classes.Games
-session = Session(engine)
+DATABASE = "SteamData_Dummy"
+class Database(object):
+    def __init__(self) -> None:
+        self.conn = sqlite3.connect(DATABASE)
+        self.cur = self.conn.cursor()
+
+    def __exit__(self, ext_type, exc_value, traceback):
+        self.cur.close()
+        if isinstance(exc_value, Exception):
+            self.connection.rollback()
+        else:
+            self.connection.commit()
+            self.connection.close()
+
+    def insert_Games(self, values:tuple):
+        """Order of the columns:
+            gameID, gamename, releaseDate, languageEnglish,
+            requiredAge, gameAchievements, positiveRatings,
+            negativeRatings, averagePlaytime, medianPlaytime,
+            gamePrice, gameOwners
+        """
+        try:
+            conn.execute("""INSERT INTO Games
+                           (gameID, gameName,
+                            releaseDate, languageEnglish,
+                            requiredAge, gameAchievements,
+                            positiveratings, negativeRatings,
+                            averagePlaytime, medianPlaytime, gamePrice)
+                            Values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""",
+                            values)
+        except sqlite3.Error:
+            print("Something went wrong!")
+    
+    def insert_Developers(self):
+        pass
+
+conn = sqlite3.connect(DATABASE)
+cur = conn.cursor()
 
 with open("steam.csv", mode="r", encoding="utf-8") as file:
     csv_reader = csv.reader(file)
     for row in csv_reader:
         if row[0].isdigit() == True:
-            stmt = (
-                insert(Games).values(
-                    gameID = row[0],
-                    gameName = row[1],
-                    releaseDate = row[2],
-                    languageEnglish = row[3],
-                    requiredAge = row[7],
-                    gameAchievements = row[11],
-                    positiveRatings = row[12],
-                    negativeRatings = row[13],
-                    averagePlaytime = row[14],
-                    medianPlaytme = row[15],
-                    gamePrice = row[17],
-                    )
-                )
+            cur.execute("""INSERT INTO Games (gameID, gameName,
+                                                  releaseDate, languageEnglish,
+                                                  requiredAge, gameAchievements,
+                                                  positiveratings, negativeRatings,
+                                                  averagePlaytime, medianPlaytime, gamePrice)
+                                           Values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""",
+                            (row[0], row[1], row[2], row[3], row[7], row[11],
+                             row[12], row[13], row[14], row[15], row[17])
+                            )
+            
             
