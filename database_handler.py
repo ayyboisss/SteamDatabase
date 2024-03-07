7fcd7ad4ec76c9037b347a543cd2390a13298d06
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-03-08 10:19:06 +1300

    Database handler v3

---
+++ database_handler.py
@@ -0,0 +1,77 @@
+import sqlite3
+
+class Database(object):
+    """Remember to commit()!!!"""
+
+    def set_Database(self, DATABASE):
+        self.DATABASE = DATABASE
+        self.conn = sqlite3.connect(self.DATABASE)
+        self.cur = self.conn.cursor()
+
+    def __exit__(self, ext_type, exc_value, traceback):
+        self.cur.close()
+        if isinstance(exc_value, Exception):
+            self.conn.rollback()
+        else:
+            self.conn.commit()
+            self.conn.close()
+
+    def commit(self):
+        self.conn.commit()
+
+    def insert_Games(self, values:tuple):
+        """Order of the columns:
+            gameID, gamename, releaseDate, languageEnglish,
+            requiredAge, gameAchievements, positiveRatings,
+            negativeRatings, averagePlaytime, medianPlaytime,
+            gamePrice, gameOwners
+        """
+        try:
+            self.cur.execute("""
+                                INSERT INTO Games
+                                (gameID, gameName,
+                                releaseDate, languageEnglish,
+                                requiredAge, gameAchievements,
+                                positiveratings, negativeRatings,
+                                averagePlaytime, medianPlaytime, gamePrice)
+                                Values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                            """,
+                            values)
+        except sqlite3.Error:
+            print("Something went wrong!")
+    
+    def insert_Developers(self, developerName=str):
+        self.cur.execute("""
+                          INSERT INTO Developers (developerName)
+                          Values (?)
+                          """, (developerName))
+    
+    def insert_Categories(self, categoryName):
+        self.cur.execute("""
+                          INSERT INTO Categories (categoryName)
+                          Values (?)
+                         """, (categoryName))
+    
+    def insert_Tags(self, tagName):
+        self.cur.execute("""
+                          INSERT INTO Tags (tagName)
+                          Values (?)
+                         """, (tagName))
+    
+    def insert_Pubishers(self, publisherName):
+        self.cur.execute("""
+                          INSERT INTO Publishers (publisherName)
+                          VALUES (?)
+                         """, (publisherName))
+    
+    def insert_Owners(self, ownerAmount):
+        self.cur.execute("""
+                          INSERT INTO Owners (ownerAmount)
+                         VALUES (?)
+                         """, (ownerAmount))
+    
+    def select_all(self, QUERY):
+        """A function for debugging, REMOVE after finishing this"""
+        self.cur.execute(QUERY)
+        result = self.cur.fetchall()
+        return result
